356f32cd2d96585af3ed239cc0f10be1e24d68ee
dagem12 <0zerocool99@gmail.com>
2025-05-14 10:07:54 +0300

    aggregation and window functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,3 +7,28 @@ from
 	booking b
 group by
 	b.user_id;
+
+
+--Rank properties based on the total number of bookings they have received.
+WITH property_bookings AS (
+    SELECT 
+        p.property_id, 
+        p.name, 
+        COUNT(b.booking_id) AS booking_count
+    FROM 
+        property p
+    LEFT JOIN 
+        booking b ON p.property_id = b.property_id
+    GROUP BY 
+        p.property_id, p.name
+)
+SELECT 
+    property_id, 
+    name, 
+    booking_count,
+    ROW_NUMBER() OVER (ORDER BY booking_count DESC) AS row_num,
+    RANK() OVER (ORDER BY booking_count DESC) AS `rank`
+FROM 
+    property_bookings
+ORDER BY 
+    booking_count DESC;
